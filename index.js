a202377837ef0da174eb8118d5b0f922f46461f5
TheDarkSid3r <sid3rdark@gmail.com>
2021-09-14 20:42:22 -0700

    history

--- index.js
+++ index.js
@@ -3,22 +3,35 @@ window.addEventListener("load", () => {
     const generateButton = document.querySelector(".generate");
     const prompt = document.querySelector(".prompt");
     const bumper = document.querySelector(".prompt-bumper");
+    const historyContent = document.querySelector(".history>.content");
     const BLANK = "_".repeat(7);
 
-    let currentPrompt = null;
-    const update = () => {
-        if (currentPrompt) {
-            prompt.innerHTML = currentPrompt.fields.find(f => f.n == "QuestionText").v.replace(/<BLANK>/gi, BLANK).replace(/<PLAYER>/gi, playername.value.trim().toUpperCase() || BLANK);
-            bumper.innerHTML = currentPrompt.personal.replace(/<BLANK>/gi, BLANK);
-        } else {
-            prompt.innerHTML = "--";
-            bumper.innerHTML = "--";
-        }
+    const formatPrompt = p => {
+        return {
+            prompt: p && p.prompt ? p.prompt.fields.find(f => f.n == "QuestionText").v.replace(/<BLANK>/gi, BLANK).replace(/<PLAYER>/gi, p.playername.trim().toUpperCase() || BLANK) : "--",
+            personal: p && p.prompt ? p.prompt.personal.replace(/<BLANK>/gi, BLANK) : "--"
+        };
     };
-    update();
-    playername.addEventListener("input", update);
-    generateButton.addEventListener("click", () => {
-        currentPrompt = TMIData.content[Math.floor(Math.random() * TMIData.content.length)];
+
+    const history = [];
+    const update = () => {
+        const last = history[history.length - 1];
+        last.playername = playername.value;
+        const p = formatPrompt(last);
+        prompt.innerHTML = p.prompt;
+        bumper.innerHTML = p.personal;
+        last.entry.innerHTML = p.prompt;
+    };
+    const generateRandom = () => {
+        const entry = document.createElement("div");
+        entry.className = "entry";
+        historyContent.appendChild(entry);
+        let prompt = null;
+        while (!prompt || (history.length && prompt.id == history[history.length - 1].id)) prompt = TMIData.content[Math.floor(Math.random() * TMIData.content.length)];
+        history.push({ prompt, entry });
         update();
-    });
+    };
+    generateRandom();
+    playername.addEventListener("input", update);
+    generateButton.addEventListener("click", generateRandom);
 });
